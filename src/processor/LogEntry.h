6c05cc7d6a8f5e426ec0d09ddcefabccb069450c
Kees Spoelstra <kees@ubuntu.WE-AMP.COM>
2013-11-27 12:09:36 +0100

    keesspoelstra-botbanger-hitmiss

---
+++ src/processor/LogEntry.h
@@ -0,0 +1,24 @@
+#ifndef LOGENTRY_H
+#define LOGENTRY_H
+#include <stdio.h>
+#include <time.h>
+
+
+#define UNUSED(x) (void)(x)
+
+enum CacheLookupStatus {Hit=0,Miss=1,Stale=2,Error=3};
+struct LogEntry
+{
+	char hostname[100];
+	char userAgent[120];
+	char url[400]; // just cut off after 400
+	time_t endTime;
+	long msDuration;
+	int httpCode;
+	int payloadsize;
+	int requestDepth;
+	CacheLookupStatus cacheLookupStatus;
+	char useraddress[40];
+	char contenttype[80];
+};
+#endif
---
+++ src/processor/utils/timegm.h
@@ -0,0 +1,1 @@
+time_t internal_timegm(struct tm *t);
