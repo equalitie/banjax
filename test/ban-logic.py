40b55ab0d08328655be5885d8c3a92f55019f928
Peter Jankuliak <p.jankuliak@gmail.com>
2018-01-02 16:33:18 +0100

    Fix and simplify banning logic in tests/ban-logic.py

--- test/ban-logic.py
+++ test/ban-logic.py
@@ -3,23 +3,14 @@
 import time
 
 class State:
-    begin             = None
-    rate              = None
-    hits_per_interval = None
-    interval          = None
-    max_rate          = None
-    banned            = False
-
-    def __init__(self, hits_per_interval = None, interval = None):
-        self.begin = 0.0
-        self.rate = 0.0
-        self.hits_per_interval = float(hits_per_interval)
-        self.interval = float(interval)
-        self.max_rate = self.hits_per_interval / self.interval
-
+    def __init__(self, max_hit_count = None, interval = None):
+        self.begin         = 0.0
+        self.hit_count     = 0.0
+        self.max_hit_count = float(max_hit_count)
+        self.interval      = float(interval)
+        self.banned        = False
 
     def ban(self):
-        #print "Banned!\n"
         self.banned = True
 
 
@@ -30,7 +21,7 @@ def handle_request(state, cur_time):
         state.begin = cur_time
 
     #
-    #         time_window_movement
+    #     time_window_movement
     #         <----->
     #         |     |
     #         |     |                       cur_time
@@ -42,42 +33,34 @@ def handle_request(state, cur_time):
     #
     time_window_movement = cur_time - state.begin - state.interval
 
-    # Same as `if (cur_time - begin) > state.interval`.
-    # That is, "if the time of our measurement exceeded the state.interval"
     if time_window_movement > 0:
         # Situation:
-        # ... -------x-----<----------------------------->
         # I.e. the interval we've measured is bigger than state.interval.
-        # So in the next line we "move" begin so that
+        # ... -------x-----<----------------------------->
+
+        # Add one hit
+        state.hit_count += 1
+
+        # "Cut out" a portion of hits_count corresponding to time_window_movement
+        state.hit_count -= state.hit_count * time_window_movement / (cur_time - state.begin)
+
+        # In the next line we "move" begin so that
         # (cur_time - begin) is equal to state.interval.
         # Or graphically, our situation will become like this:
         #                  x
         # ... -------------<----------------------------->
         state.begin = state.begin + time_window_movement
-        # Now, since we "cut out" the time_window_movement part of our
-        # working interval, we need to adjust our rate accordingly.
-        # I'll break the original formula:
-        # rate = rate - (rate * time_window_movement - 1) / state.interval
-        # into two:
-        # 1. "Cut out" a portion of rate corresponding to time_window_movement
-        state.rate = state.rate - state.rate * time_window_movement / state.interval
-        # and:
-        # 2. Add one hit
-        state.rate += 1/state.interval
-        # I think this is just a floating point error correction to ensure
-        # rate is not less than zero.
-        if state.rate < 0:
-            state.rate = 1/state.interval
+
+        # Floating point error correction to ensure hit_count is not negative.
+        state.hit_count = max(0, state.hit_count)
     else:
         # Situation:
         # ......... -------<-----x----------------------->
         # That is, we haven't yet measured for more than 
         # state.interval time. So just increment rate by adding one hit.
-        state.rate += 1/state.interval
+        state.hit_count += 1
 
-    #print("rate:" + repr(state.rate) + " max_rate:" + repr(state.max_rate))
-
-    if state.rate >= state.max_rate:
+    if state.hit_count >= state.max_hit_count:
         state.ban()
 
 
@@ -85,25 +68,25 @@ def now():
     return time.time()
 
 def test_1():
-    s = State(hits_per_interval = 5, interval = 5)
+    s = State(max_hit_count = 5, interval = 5)
 
     HITS = 5
 
-    for hit in range(0, HITS):
+    for hit in range(1, HITS + 1):
         handle_request(s, float(hit) / 2)
 
-        if hit < 4: # 0,1,2,3
+        if hit < 5:
             assert s.banned == False
-        else: # 4
+        else:
             assert s.banned == True
 
 
 def test_2():
-    s = State(hits_per_interval = 30, interval = 60)
+    s = State(max_hit_count = 30, interval = 60)
 
-    HITS = 31
+    HITS = 30
 
-    for hit in range(0, HITS):
+    for hit in range(1, HITS + 1):
         handle_request(s, float(hit))
 
         if hit < 30:
@@ -112,13 +95,12 @@ def test_2():
             assert s.banned == True
 
 
-
 if __name__ == "__main__":
     test_1()
     test_2()
 
-    # Uncomment if you wish to test this manually.
-    #s = State(hits_per_interval = 5.0, interval = 5.0)
+    ## Uncomment if you wish to test this manually.
+    #s = State(max_hit_count = 5.0, interval = 5.0)
     #while s.banned == False:
     #    raw_input("Hit enter to simulate a request")
     #    handle_request(s, now())
