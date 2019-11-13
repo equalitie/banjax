4ac3072807a91747131d1be866b6ec099027e876
Peter Jankuliak <p.jankuliak@gmail.com>
2019-11-13 15:25:38 +0100

    regex_manager.cpp: consistent padding

--- src/regex_manager.cpp
+++ src/regex_manager.cpp
@@ -68,79 +68,79 @@ RegexManager::parse_request(string ip, string ats_record) const
 
   for(const auto& rule : rated_banning_regexes) {
     if (RE2::FullMatch(ats_record, *(rule->re2_regex))) {
-        TSDebug(BANJAX_PLUGIN_NAME, "requests matched %s", (rule->re2_regex->pattern()).c_str());
-        //if it is a simple regex i.e. with rate 0 we bans immidiately without
-        //wasting time and mem
-        if (rule->rate == 0) {
-          TSDebug(BANJAX_PLUGIN_NAME, "simple regex, ban immediately");
-          return make_pair(REGEX_MATCHED, rule.get());
-        }
-        //select appropriate rate, dependent on whether GET or POST request
+      TSDebug(BANJAX_PLUGIN_NAME, "requests matched %s", (rule->re2_regex->pattern()).c_str());
+      //if it is a simple regex i.e. with rate 0 we bans immidiately without
+      //wasting time and mem
+      if (rule->rate == 0) {
+        TSDebug(BANJAX_PLUGIN_NAME, "simple regex, ban immediately");
+        return make_pair(REGEX_MATCHED, rule.get());
+      }
+      //select appropriate rate, dependent on whether GET or POST request
 
-        /* we need to check the rate condition here */
-        //getting current time in msec
-        timeval cur_time; gettimeofday(&cur_time, NULL);
-        long cur_time_msec = cur_time.tv_sec * 1000 + cur_time.tv_usec / 1000.0;
+      /* we need to check the rate condition here */
+      //getting current time in msec
+      timeval cur_time; gettimeofday(&cur_time, NULL);
+      long cur_time_msec = cur_time.tv_sec * 1000 + cur_time.tv_usec / 1000.0;
 
-        /* first we check if we already have retreived the state for this ip */
-        if (!ip_state || ip_state->size() == 0) {
-          ip_state =  regex_manager_ip_db->get_ip_state(ip);
-          if (!ip_state) //we failed to read the database we make no judgement
-            continue;
-        }
+      /* first we check if we already have retreived the state for this ip */
+      if (!ip_state || ip_state->size() == 0) {
+        ip_state =  regex_manager_ip_db->get_ip_state(ip);
+        if (!ip_state) //we failed to read the database we make no judgement
+          continue;
+      }
 
-        //if we succeeded in retreiving but the size is zero then it meaens we don't have a record of this ip at all
-        if (ip_state->size() == 0) {
-          ip_state->resize(total_no_of_rules);
-        }
+      //if we succeeded in retreiving but the size is zero then it meaens we don't have a record of this ip at all
+      if (ip_state->size() == 0) {
+        ip_state->resize(total_no_of_rules);
+      }
 
-        //now we check the begining of the hit for this regex (if the vector is just created everything is 0, in case
-        //this is the first regex this ip hits)
-        RegexState state = (*ip_state)[rule->id];
+      //now we check the begining of the hit for this regex (if the vector is just created everything is 0, in case
+      //this is the first regex this ip hits)
+      RegexState state = (*ip_state)[rule->id];
 
-        //if it is 0, then we don't have a record for
-        //the current regex
-        if (state.begin_msec == 0) {
-          state.begin_msec = cur_time_msec;
-        }
+      //if it is 0, then we don't have a record for
+      //the current regex
+      if (state.begin_msec == 0) {
+        state.begin_msec = cur_time_msec;
+      }
 
-        //now we have a record, update the rate and ban if necessary.
-        //we move the interval by the differences of the "begin_in_ms - cur_time_msec - interval*1000"
-        //if it is less than zero we don't do anything just augument the rate
-        long time_window_movement = cur_time_msec - state.begin_msec - rule->interval;
-        if (time_window_movement > 0) { //we need to move
-           state.begin_msec += time_window_movement;
-           state.rate = state.rate - (state.rate * time_window_movement - 1)/(double) rule->interval;
-           state.rate = state.rate < 0 ? 1/(double) rule->interval : state.rate; //if time_window_movement > time_window_movementrule->interval, then we just wipe history and start as a new interval
-         }
-         else {
-           //we are still in the same interval so just increase the hit by 1
-           state.rate += 1/(double) rule->interval;
-         }
+      //now we have a record, update the rate and ban if necessary.
+      //we move the interval by the differences of the "begin_in_ms - cur_time_msec - interval*1000"
+      //if it is less than zero we don't do anything just augument the rate
+      long time_window_movement = cur_time_msec - state.begin_msec - rule->interval;
+      if (time_window_movement > 0) { //we need to move
+         state.begin_msec += time_window_movement;
+         state.rate = state.rate - (state.rate * time_window_movement - 1)/(double) rule->interval;
+         state.rate = state.rate < 0 ? 1/(double) rule->interval : state.rate; //if time_window_movement > time_window_movementrule->interval, then we just wipe history and start as a new interval
+       }
+       else {
+         //we are still in the same interval so just increase the hit by 1
+         state.rate += 1/(double) rule->interval;
+       }
 
-        TSDebug(BANJAX_PLUGIN_NAME, "with rate %f /msec", state.rate);
+      TSDebug(BANJAX_PLUGIN_NAME, "with rate %f /msec", state.rate);
 
-        (*ip_state)[rule->id] = state;
+      (*ip_state)[rule->id] = state;
 
-        if (state.rate >= rule->rate) {
-          TSDebug(BANJAX_PLUGIN_NAME, "exceeding excessive rate %f /msec", rule->rate);
-          //clear the record to avoid multiple reporting to swabber
-          //we are not clearing the state cause it is not for sure that
-          //swabber ban the ip due to possible failure of acquiring lock
-          // ip_state.detail.begin_msec = 0;
-          // ip_state.detail.rate = 0;
-          // regex_manager_ip_db->set_ip_state(ip, ip_state.state_allocator);
+      if (state.rate >= rule->rate) {
+        TSDebug(BANJAX_PLUGIN_NAME, "exceeding excessive rate %f /msec", rule->rate);
+        //clear the record to avoid multiple reporting to swabber
+        //we are not clearing the state cause it is not for sure that
+        //swabber ban the ip due to possible failure of acquiring lock
+        // ip_state.detail.begin_msec = 0;
+        // ip_state.detail.rate = 0;
+        // regex_manager_ip_db->set_ip_state(ip, ip_state.state_allocator);
 
-          //before calling swabber we need to delete the memory as swabber will
-          //delete the ip database entery and the memory will be lost.
-          //However, if swabber fails to acquire a lock this means
-          //the ip can escape the banning, this however is a miner
-          //concern compared to the fact that we might ran out of
-          //memory.
+        //before calling swabber we need to delete the memory as swabber will
+        //delete the ip database entery and the memory will be lost.
+        //However, if swabber fails to acquire a lock this means
+        //the ip can escape the banning, this however is a miner
+        //concern compared to the fact that we might ran out of
+        //memory.
 
-          regex_manager_ip_db->set_ip_state(ip, *ip_state);
-          return make_pair(REGEX_MATCHED, rule.get());
-        }
+        regex_manager_ip_db->set_ip_state(ip, *ip_state);
+        return make_pair(REGEX_MATCHED, rule.get());
+      }
     }
   }
 
